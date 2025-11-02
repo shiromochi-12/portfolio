9b8785fb931dfdfcdd0b3129a02820ec0ba9f810
shiromochi-12 <aaa159xyz@gmail.com>
2025-11-02 10:56:11 +0900

    Add files via upload

---
+++ JS/main.js
@@ -0,0 +1,12 @@
+$(function () {
+
+/*スムーススクロール*/
+ 
+  $('a[href^="#"]').click(function () {
+    let href = $(this).attr("href");
+    let target = $(href == "#" || href == "" ? "html" : href);
+    let position = target.offset().top;
+    $("html, body").animate({ scrollTop: position }, 2000, "swing");
+    return false;
+  });
+});
